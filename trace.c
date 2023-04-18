a531cf132c88464eb7476e9e9329b5511c233326
Aladdin A. Ismael <aaismael@unix3.csc.calpoly.edu>
2023-04-17 17:42:40 -0700

    Most 'done' (not fully tested) except for TCP

--- trace.c
+++ trace.c
@@ -18,7 +18,7 @@
 // incorrect arglist length
 #define ARGL -4
 
-// internet protocol values
+// Protocol values
 // Internet Layer
 #define IP 2048
 #define ARP 2054
@@ -27,10 +27,19 @@
 #define TCP 6
 #define UDP 17
 
-int extractEthernetHeader(const unsigned char* pkt_data, int pkt_number, struct pcap_pkthdr* pkt_header);
-int extractIPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header);
+#define ETH_HEADER_LENGTH 14
 
-char* getProtocolValue(unsigned char value);
+int extractEthernetHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header);
+
+int extractIPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header);
+void extractARPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header);
+
+void extractICMPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header, int cursor);
+void extractUDPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header, int cursor);
+void extractTCPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header, int cursor);
+
+char* getTransportProtocol(unsigned char value);
+int isValidIPChecksum(const unsigned char* pkt_data, int header_length, int cursor);
 
 int main(int argc, char *argv[]) {
 
@@ -67,15 +76,29 @@ int main(int argc, char *argv[]) {
 
     // read every pcap file
     while((res = pcap_next_ex(save_file, &pkt_header, &pkt_data)) == 1) {
-        int internet_type = extractEthernetHeader(pkt_data, pkt_number, pkt_header);
-        int offset;
+
+        printf("\nPacket number: %d  Frame Len: %d\n\n", pkt_number, pkt_header->len);
+
+        int internet_type = extractEthernetHeader(pkt_data, pkt_header);
+        int data_offset;
         // if IP Header
         if(internet_type == IP) {
-            offset = extractIPHeader(pkt_data, pkt_header);
+            data_offset = extractIPHeader(pkt_data, pkt_header);
+            // check protocol type
+            // if ICMP
+            if(pkt_data[23] == ICMP) {
+                extractICMPHeader(pkt_data, pkt_header, data_offset + ETH_HEADER_LENGTH);
+            }
+            else if(pkt_data[23] == UDP) {
+                extractUDPHeader(pkt_data, pkt_header, data_offset + ETH_HEADER_LENGTH);
+            }
+            else if(pkt_data[23] == TCP) {
+                extractTCPHeader(pkt_data, pkt_header, data_offset + ETH_HEADER_LENGTH);
+            }
         }
         // if ARP Header
         if(internet_type == ARP) {
-            
+            extractARPHeader(pkt_data, pkt_header);
         }
 
 
@@ -93,23 +116,12 @@ int main(int argc, char *argv[]) {
 }
 
 // extract ethernet header and frame information. returns protocol type (e.g. IP, ARP).
-int extractEthernetHeader(const unsigned char* pkt_data, int pkt_number, struct pcap_pkthdr* pkt_header) {
-
-    printf("Packet number: %d  Frame Len: %d\n\n", pkt_number, pkt_header->len);
+int extractEthernetHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header) {
     
     printf("\tEthernet Header\n");
-    printf("\t\tDest MAC: ");
-    for (int i = 0; i < 6; i++) {
-        if(i < 5)
-            printf("%x:", pkt_data[i]);
-        else printf("%x", pkt_data[i]);
-    }
-    printf("\n\t\tSource MAC: ");
-    for (int i = 6; i < 12; i++) {
-        if(i < 11)
-            printf("%x:", pkt_data[i]);
-        else printf("%x", pkt_data[i]);
-    }
+    printf("\t\tDest MAC: %s\n",ether_ntoa((struct ether_addr *)&pkt_data[0]));
+    printf("\t\tSource MAC: %s\n", ether_ntoa((struct ether_addr *)&pkt_data[6]));
+    
     unsigned short type_value;
     memcpy(&type_value, &pkt_data[12], 2);
     type_value = ntohs(type_value);
@@ -121,7 +133,7 @@ int extractEthernetHeader(const unsigned char* pkt_data, int pkt_number, struct 
         type = "ARP";
     }
         
-    printf("\n\t\tType: %s\n\n", type);
+    printf("\t\tType: %s\n\n", type);
 
     return type_value;
 }
@@ -129,45 +141,154 @@ int extractEthernetHeader(const unsigned char* pkt_data, int pkt_number, struct 
 int extractIPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header) {
 
     // IP Header starting point
-    int cursor = 14;
+    int cursor = ETH_HEADER_LENGTH;
 
-    int header_length = pkt_data[cursor] & 0x0F * 4;
+    int header_length = (pkt_data[cursor] & 0x0F) * 4;
     unsigned char protocol_value = pkt_data[cursor + 9];
-    char* protocol = getProtocolValue(protocol_value);
+    char* protocol = getTransportProtocol(protocol_value);
 
     printf("\tIP Header\n");
     printf("\t\tHeader Len: %d (bytes)\n", header_length);
     printf("\t\tTOS: 0x%x\n", pkt_data[cursor + 1]);
     printf("\t\tTTL: %d\n", pkt_data[cursor + 8]);
-    printf("\t\tIP PDU Len: %d (bytes)\n", pkt_data[cursor + 3]);
+
+    short pdu_length;
+    memcpy(&pdu_length, &pkt_data[cursor + 2], 2);
+    pdu_length = ntohs(pdu_length);
+    printf("\t\tIP PDU Len: %d (bytes)\n", pdu_length);
+
     printf("\t\tProtocol: %s\n", protocol);
 
-    if(in_cksum((unsigned short *) &pkt_data[cursor + 10], 2) == 0) {
-        printf("\t\tChecksum: Correct (0x%x%x)\n", pkt_data[cursor + 11], pkt_data[cursor + 10]);
+    // validate checksum
+    if(isValidIPChecksum(pkt_data, header_length, cursor) == 0) {
+        unsigned short checksum;
+        memcpy(&checksum, &pkt_data[cursor + 10], 2);
+        printf("\t\tChecksum: Correct (0x%x)\n", checksum);
     }
-    else
-        printf("\t\tChecksum: Incorrect (0x%x%x)\n", pkt_data[cursor + 11], pkt_data[cursor + 10]);
-
-    printf("\t\tSender IP: ");
-    for(int i = cursor + 12; i < cursor + 16; i++) {
-        if(i < cursor + 15)
-            printf("%d.",pkt_data[i]);
-        else 
-            printf("%d\n",pkt_data[i]);
+    else {
+        unsigned short checksum;
+        memcpy(&checksum, &pkt_data[cursor + 10], 2);
+        printf("\t\tChecksum: Incorrect (0x%x)\n", checksum);
     }
 
-    printf("\t\tDest IP: ");
-    for(int i = cursor + 16; i < cursor + 20; i++) {
-        if(i < cursor + 19)
-            printf("%d.",pkt_data[i]);
-        else 
-            printf("%d\n\n",pkt_data[i]);
-    }
+    // get sender and destination IPs
+    struct in_addr address_buff;
+    memcpy(&address_buff, &pkt_data[cursor + 12], sizeof(struct in_addr));
+    printf("\t\tSender IP: %s\n", inet_ntoa(address_buff));
+    memcpy(&address_buff, &pkt_data[cursor + 16], sizeof(struct in_addr));
+    printf("\t\tDest IP: %s\n\n", inet_ntoa(address_buff));
 
     return header_length;
 }
 
-char* getProtocolValue(unsigned char value) {
+void extractARPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header) {
+
+    // ARP Header starting point
+    int cursor = ETH_HEADER_LENGTH;
+
+    printf("\tARP header\n");
+    // get opcode
+    if(pkt_data[cursor + 7] == 1)
+        printf("\t\tOpcode: Request\n");
+    else if (pkt_data[cursor + 7] == 2)
+        printf("\t\tOpcode: Reply\n");
+    else
+        printf("\t\tOpcode: Not a valid opcode: %d\n", pkt_data[cursor + 7]);
+
+    // get sender and target MACs and IPs
+    printf("\t\tSender MAC: %s\n", ether_ntoa((struct ether_addr *)&pkt_data[cursor + 8]));
+
+    struct in_addr address_buff;
+    memcpy(&address_buff, &pkt_data[cursor + 14], sizeof(struct in_addr));
+    printf("\t\tSender IP: %s\n", inet_ntoa(address_buff));
+
+    printf("\t\tTarget MAC: %s\n",ether_ntoa((struct ether_addr *)&pkt_data[cursor + 18]));
+
+    memcpy(&address_buff, &pkt_data[cursor + 24], sizeof(struct in_addr));
+    printf("\t\tTarget IP: %s\n\n", inet_ntoa(address_buff));
+    
+}
+
+void extractICMPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header, int cursor) {
+    printf("\tICMP Header\n");
+    if(pkt_data[cursor] == 8)
+        printf("\t\tType: Request\n\n");
+    else if(pkt_data[cursor] == 0)
+        printf("\t\tType: Reply\n\n");
+    else
+        printf("\t\tType: Invalid Type\n");
+}
+
+void extractUDPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header, int cursor) {
+    printf("\tUDP Header\n");
+    // get port numbers
+    unsigned short port;
+    memcpy(&port, &pkt_data[cursor], 2);
+    port = ntohs(port);
+    printf("\t\tSource Port: : %d\n", port);
+    memcpy(&port, &pkt_data[cursor + 2], 2);
+    port = ntohs(port);
+    printf("\t\tDest Port: : %d\n", port);
+}
+
+void extractTCPHeader(const unsigned char* pkt_data, struct pcap_pkthdr* pkt_header, int cursor) {
+    printf("\tTCP Header\n");
+    // get port numbers
+    unsigned short port;
+    memcpy(&port, &pkt_data[cursor], 2);
+    port = ntohs(port);
+    printf("\t\tSource Port: : %d\n", port);
+    memcpy(&port, &pkt_data[cursor + 2], 2);
+    port = ntohs(port);
+    printf("\t\tDest Port: : %d\n", port);
+
+    // get sequence number
+    unsigned long seq_num;
+    memcpy(&seq_num, &pkt_data[cursor + 4], 4);
+    seq_num = ntohl(seq_num);
+    printf("\t\tSequence Number: %lu\n", seq_num);
+
+    // check flags
+    unsigned char flags = pkt_data[cursor + 13];
+    // check ACK flag and print ACK number accordingly
+    if((flags & 0x10) == 0x10) {
+        unsigned long ack_num;
+        memcpy(&ack_num, &pkt_data[cursor + 8], 4);
+        ack_num = ntohl(ack_num);
+        printf("\t\tACK Number: %lu\n", ack_num);
+        printf("\t\tACK Flag: Yes\n");
+    }
+    else {
+        printf("\t\tACK Number: <not valid>\n");
+        printf("\t\tACK Flag: No\n");
+    }
+
+    // check rest of flags
+    if((flags & 0x2) == 0x2) {
+        printf("\t\tSYN Flag: Yes\n");
+    }
+    else printf("\t\tSYN Flag: No\n");
+    if((flags & 0x4) == 0x4) {
+        printf("\t\tRST Flag: Yes\n");
+    }
+    else printf("\t\tRST Flag: No\n");
+    if((flags & 0x1) == 0x1) {
+        printf("\t\tFIN Flag: Yes\n");
+    }
+    else printf("\t\tFIN Flag: No\n");
+    
+    // window size
+    unsigned short window_size;
+    memcpy(&window_size, &pkt_data[cursor + 14], 2);
+    window_size = ntohs(window_size);
+    printf("\t\tWindow Size: %hu\n", window_size);
+    
+    // TCP pseudoheader checksum
+
+
+}
+
+char* getTransportProtocol(unsigned char value) {
     if(value == ICMP)
         return "ICMP";
     else if(value == TCP)
@@ -176,5 +297,38 @@ char* getProtocolValue(unsigned char value) {
         return "UDP";
     else
         return "protocol not defined in this program";
+}
 
+// Checks if IP header checksum is valid. returns 0 on true, anything else on false.
+int isValidIPChecksum(const unsigned char* pkt_data, int header_length, int cursor) {
+    // copy header data
+    unsigned char* checksum_buff = malloc(sizeof(unsigned char) * header_length);
+    memcpy(checksum_buff, &pkt_data[cursor], header_length);
+    
+    int result = in_cksum((unsigned short*) checksum_buff, header_length);
+
+    free(checksum_buff);
+    return result;
+}
+
+// Checks if TCP header checksum is valid. returns 0 on true, anything else on false.
+int isValidTCPChecksum(const unsigned char* pkt_data, int header_length, int cursor) {
+    // copy header data
+    unsigned char* checksum_buff = malloc(sizeof(unsigned char) * header_length);
+    memcpy(checksum_buff, &pkt_data[cursor], header_length);
+
+    // find length needed for checksum buffer
+    int length;
+    // pseudo-header
+    length = 96;
+    // tcp header length
+    length += (pkt_data[cursor + 12] & 0x0F) * 4;
+    // tcp checksum
+    
+    
+
+    int result = in_cksum((unsigned short*) checksum_buff, header_length);
+
+    free(checksum_buff);
+    return result;
 }
